511b674c951031492bdfaf42f8628db4890371e5
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2022-11-15 17:34:37 -0800

    Update __init__.py

--- Fair-Clustering-Codebase/fair_clustering/dataset/__init__.py
+++ Fair-Clustering-Codebase/fair_clustering/dataset/__init__.py
@@ -1,9 +1,4 @@
 from fair_clustering.dataset.base import Dataset, TabDataset, ImageDataset
-from fair_clustering.dataset.adult import Adult
-from fair_clustering.dataset.compas import Compas
-from fair_clustering.dataset.german import German
-from fair_clustering.dataset.bank import Bank
-from fair_clustering.dataset.credit import Credit
 from fair_clustering.dataset.mnist_data import MNISTData, MNIST_USPS
 from fair_clustering.dataset.extended_yaleB import ExtendedYaleB
 from fair_clustering.dataset.office31 import Office31
